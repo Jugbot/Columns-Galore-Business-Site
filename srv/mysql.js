e908bb2242aa39fa040f5ab8c0eb4ae94e67ce5b
Lucas <lp1935@nyu.edu>
2020-06-06 14:21:03 -0400

    detach server

--- srv/mysql.js
+++ srv/mysql.js
@@ -1,11 +1,11 @@
 import mysql from 'mysql'
 
 var connection = mysql.createConnection({
-  host: 'localhost',
-  // port: '3306',
-  user: 'root',
-  password: 'password',
-  database: 'columnsgalore'
+  host: process.env.HOST || 'localhost',
+  port: process.env.PORT || '3306',
+  user: process.env.USER || 'root',
+  password: process.env.PASSWORD || 'password',
+  database: process.env.DATABASE || 'columnsgalore'
 })
 
 connection.connect(function (err) {
