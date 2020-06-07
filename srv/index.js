751b06de90be337f6681e14b6e3db47930b6f4bf
Lucas <lp1935@nyu.edu>
2020-06-06 23:17:14 -0400

    f*** CORS

--- srv/index.js
+++ srv/index.js
@@ -16,7 +16,7 @@ function objectToWhereValues (object) {
 };
 
 function server (app, http) {
-  app.use(cors({ origin: ['http://localhost:8080', /columnsgalore\.com$/] }))
+  app.use(cors({ origin: /columnsgalore\.com$/ }))
   app.use(express.json())
 
   let catalogQueries = [
